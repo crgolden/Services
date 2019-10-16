5582ed5dcaaa7cd70efd49d83c7e96afb1f527f6
Chris Golden <crgolden@msn.com>
2019-10-15 19:38:23 -0500

    Updating packages

---
+++ Data.EntityFramework/HostExtensions.cs
@@ -0,0 +1,24 @@
+﻿namespace Services
+{
+    using System.Threading;
+    using System.Threading.Tasks;
+    using Microsoft.EntityFrameworkCore;
+    using Microsoft.Extensions.DependencyInjection;
+    using Microsoft.Extensions.Hosting;
+
+    public static class HostExtensions
+    {
+        public static async Task<IHost> MigrateDatabaseAsync(
+            this IHost host,
+            CancellationToken cancellationToken = default)
+        {
+            using (var scope = host.Services.CreateScope())
+            using (var context = scope.ServiceProvider.GetRequiredService<DbContext>())
+            {
+                await context.Database.MigrateAsync(cancellationToken).ConfigureAwait(false);
+            }
+
+            return host;
+        }
+    }
+}
