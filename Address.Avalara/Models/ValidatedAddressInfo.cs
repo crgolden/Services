c7533562da55fb63f573845e09552b7699ce49f8
Chris Golden <crgolden@msn.com>
2019-10-24 20:45:37 -0500

    Removing duplicated code

--- Address.Avalara/Models/ValidatedAddressInfo.cs
+++ Address.Avalara/Models/ValidatedAddressInfo.cs
@@ -5,36 +5,9 @@
 
     // https://developer.avalara.com/api-reference/avatax/rest/v2/models/ValidatedAddressInfo/
     [ExcludeFromCodeCoverage]
-    public class ValidatedAddressInfo
+    public class ValidatedAddressInfo : AddressInfo
     {
         [JsonPropertyName("addressType")]
         public string? AddressType { get; set; }
-
-        [JsonPropertyName("line1")]
-        public string? Line1 { get; set; }
-
-        [JsonPropertyName("line2")]
-        public string? Line2 { get; set; }
-
-        [JsonPropertyName("line3")]
-        public string? Line3 { get; set; }
-
-        [JsonPropertyName("city")]
-        public string? City { get; set; }
-
-        [JsonPropertyName("region")]
-        public string? Region { get; set; }
-
-        [JsonPropertyName("country")]
-        public string? Country { get; set; }
-
-        [JsonPropertyName("postalCode")]
-        public string? PostalCode { get; set; }
-
-        [JsonPropertyName("latitude")]
-        public double? Latitude { get; set; }
-
-        [JsonPropertyName("longitude")]
-        public double? Longitude { get; set; }
     }
 }
